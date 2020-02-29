4aade863fe3875ba4afbb65df3f1ce382525859a
Ojasvi Choudhary <48777037+ojchoudh@users.noreply.github.com>
2020-02-29 14:10:15 -0800

    Create TeamsAdapter.cs

---
+++ samples/csharp_dotnetcore/02.echo-bot/TeamsAdapter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.Teams.Middlewares;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.BotBuilderSamples
+
+{
+    public class TeamsAdapter : BotFrameworkHttpAdapter
+    {
+        public TeamsAdapter(IConfiguration configuration, ILogger<BotFrameworkHttpAdapter> logger)
+            : base(configuration, logger)
+        {
+            OnTurnError = async (turnContext, exception) =>
+            {
+                // Log any leaked exception from the application.
+                logger.LogError($"Exception caught : {exception.Message}");
+                // Send a catch-all apology to the user.
+                await turnContext.SendActivityAsync("Sorry, it looks like something went wrong.");
+            };
+            Use(new TeamsMiddleware(new ConfigurationCredentialProvider(configuration)));
+        }
+    }
+}
